e395a2010c1cf30ccec845e642c51a9fd68f241e
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-07-02 20:21:36 +0530

    Update deliveryReceiptService.js

--- frontend/src/services/deliveryReceiptService.js
+++ frontend/src/services/deliveryReceiptService.js
@@ -3,7 +3,7 @@ import API_BASE_URL from "../apiConfig";
 
 export const updateDeliveryStatus = async (logId, status) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/delivery-receipts`, {
+    const response = await axios.post(`${API_BASE_URL}/api/delivery-receipts`, {
       logId,
       status,
     });
