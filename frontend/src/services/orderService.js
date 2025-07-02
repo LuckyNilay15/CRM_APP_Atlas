f9d50cc3a97b30f9a782c5ae863cc0532dadc237
Nilay Namit <109901909+LuckyNilay15@users.noreply.github.com>
2025-07-02 20:21:57 +0530

    Update orderService.js

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -3,7 +3,7 @@ import API_BASE_URL from "../apiConfig";
 
 export const createOrder = async (orderData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/orders`, orderData);
+    const response = await axios.post(`${API_BASE_URL}/api/orders`, orderData);
     return response.data;
   } catch (error) {
     console.error("Error creating order:", error);
@@ -13,7 +13,7 @@ export const createOrder = async (orderData) => {
 
 export const fetchOrders = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/orders`);
+    const response = await axios.get(`${API_BASE_URL}/api/orders`);
     return response.data;
   } catch (error) {
     console.error("Error fetching orders:", error);
